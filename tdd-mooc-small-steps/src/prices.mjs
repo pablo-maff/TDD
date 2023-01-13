f42037189fa1138ed29b2680a7d7a53120fc7ae7
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-13 23:04:11 +0100

    reset file to try parallel change approach

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -25,7 +25,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return Temporal.PlainDate.from(dateString);
+      return new Date(dateString);
     }
   }
 
@@ -76,15 +76,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
+    return date.getDay() === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = Temporal.PlainDate.from(row.holiday)
+      let holiday = new Date(row.holiday);
       if (
-        date.equals(holiday)
+        date &&
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
       ) {
         return true;
       }
