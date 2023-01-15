9611cce1cbdd244493c573f84ec647a122f6556b
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:47:00 +0100

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -18,7 +18,6 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
     const date2 = parseTemporalDate(req.query.date)
     const cost = calculateCost(age, type, date2, baseCost);
     res.json({ cost });
