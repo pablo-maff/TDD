2b901426fbf31f80a3cb6f0246265f922027aa8f
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:22:18 +0100

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -88,6 +88,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday2 = convert(row.holiday)
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
