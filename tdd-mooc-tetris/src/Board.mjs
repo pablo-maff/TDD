09724f92dec4047f8131c03a89531e9293c3b6e9
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 22:31:07 +0100

    Falling blocks test section done

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,3 +1,5 @@
+import { Block } from "./Block.mjs";
+
 export class Board {
   width;
   height;
@@ -5,9 +7,40 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.falling = false;
+    this.ticked = 0
   }
 
   toString() {
-    return "TODO";
+    let board = '';
+    let pieceIsPlaced = false
+
+    for (let row = 0; row < this.width; row++) {
+      for (let col = 0; col < this.height; col++) {
+        if (row === this.ticked && col === 1 && this.falling && !pieceIsPlaced) {
+          board += 'X';
+          pieceIsPlaced = true
+        }
+        else {
+          board += '.';
+        }
+      }
+      board += '\n';
+    }
+    return board
+  }
+
+  drop(block) {
+    if (block.color === 'X') {
+      this.falling = true;
+    }
+    else {
+      throw new Error('already falling')
+    }
+  }
+
+  tick() {
+    this.ticked += 1
+
   }
 }
