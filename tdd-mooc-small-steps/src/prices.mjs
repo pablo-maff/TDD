31376661a5d391ea6ef4f4d26132575ebc227e7e
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:25:45 +0100

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -87,7 +87,7 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = convert(row.holiday)
+      let holiday = Temporal.PlainDate.from(row.holiday)
       if (
         date.equals(holiday)
       ) {
