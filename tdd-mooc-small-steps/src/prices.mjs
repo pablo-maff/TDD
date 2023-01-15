38f8ba2c9c09ee27a71c74b3f237b3130c37daa7
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:34:48 +0100

    file reset to complete using parallel change and tcr max changes = 2

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -1,8 +1,7 @@
 import "./polyfills.mjs";
 import express from "express";
-import { Temporal } from "@js-temporal/polyfill";
 
-// Refactor the following code to get rid of the legacy Date class. DONE
+// Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
 
 function createApp(database) {
@@ -26,7 +25,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return Temporal.PlainDate.from(dateString);
+      return new Date(dateString);
     }
   }
 
@@ -77,15 +76,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
+    return date.getDay() === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = Temporal.PlainDate.from(row.holiday)
+      let holiday = new Date(row.holiday);
       if (
-        date.equals(holiday)
+        date &&
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
       ) {
         return true;
       }
