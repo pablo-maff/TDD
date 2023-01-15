4767e4e693307d30da61b1bb7cf331698911ea41
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:44:20 +0100

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -54,7 +54,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost, date2) {
-    let reduction = calculateReduction(date, date2);
+    let reduction = calculateReduction(date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -70,7 +70,7 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date, date2) {
+  function calculateReduction(date2) {
     let reduction = 0;
     if (date2 && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
