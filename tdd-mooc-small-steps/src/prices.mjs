131f12af56335ca09ca1f3b44c99db0b6162eaca
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-15 11:46:38 +0100

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -32,11 +32,11 @@ function createApp(database) {
 
   function parseTemporalDate(dateString) {if (dateString) {return Temporal.PlainDate.from(dateString)}}
 
-  function calculateCost(age, type, date2, baseCost) {
+  function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date2, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
