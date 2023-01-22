119b37be67915dae7292e318e24c377910672e9a
pablo-maff <pmaffioli@universaldoctor.com>
2023-01-22 14:36:46 +0100

    Made stuff private in the Board class

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,23 +1,24 @@
 export class Board {
-  width;
-  height;
+  #width;
+  #height;
 
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.boardSquare = '.'
-    this.board = [];
+    this.#width = width;
+    this.#height = height;
     this.createBoard()
   }
+
+  #board = [];
+  #boardSquare = '.'
 
   /* // TODO: 
           * Clean up a bit of the mess before continuing
           * hasFalling can contain the logic to return the position of the blocks at least for the next step
           * define Block methods and use them
-          * toString should only stringify the board
+          * toString should only stringify the #board
           * drop is just the entry point for a block
           * tick only moves the block one row down
-          * After having the above working, a new method should take care of moving the blocks around the board, consider a 2D array
+          * After having the above working, a new method should take care of moving the blocks around the #board, consider a 2D array
   */
   // * LESSON 1 (* 2): If possible, make sure to know how things work before coding them!
   // * LESSON 2 (record it with fire in your mind): Each method should just take care of one thing and its name must indicate what that thing is.
@@ -27,16 +28,16 @@ export class Board {
   // * LESSON 5: Think about what you need, write down the problem, the steps to solve it and apply it
 
   /*
-  TODO: I need to detach the board creation from the placing of the pieces
-  * The board should start empty! 
+  TODO: I need to detach the #board creation from the placing of the pieces
+  * The #board should start empty! 
   TODO: REDO I need: A way to track the current position and state of a piece
   * {color: 'X', positionX: 0, positionY: 1, isFalling: true}
   TODO: REDO I need to make the piece move down
   * ticked() = positionX + 1
   TODO: REDO I need to stop the piece from falling
   * if positionX === this.ticked then isFalling: false
-  TODO: I need to detach the board creation from the board rendering
-  * Create a new method to create the board and use toString to just do that
+  TODO: I need to detach the #board creation from the #board rendering
+  * Create a new method to create the #board and use toString to just do that
   TODO: REDO I need to insert the piece in the desired position
   * Modify drop(), it should accept x and y coordinates
   */
@@ -47,28 +48,28 @@ export class Board {
   // * If a block reaches the bottom row, or it reaches the upper row of a piece in place, and is ticked one more time then the piece has stopped falling
 
   createBoard() {
-    for (let y = 0; y < this.height; y++) {
-      this.board[y] = []
-      for (let x = 0; x < this.width; x++) {
-        this.board[y][x] = this.boardSquare;
+    for (let y = 0; y < this.#height; y++) {
+      this.#board[y] = []
+      for (let x = 0; x < this.#width; x++) {
+        this.#board[y][x] = this.#boardSquare;
       }
       // * End of row
-      this.board[y][this.height] = '\n';
+      this.#board[y][this.#height] = '\n';
     }
-    // * End of board
-    return this.board
+    // * End of #board
+    return this.#board
   }
 
-  // * When toString is executed the final state of the board at the moment of execution is returned
+  // * When toString is executed the final state of the #board at the moment of execution is returned
   toString() {
-    return this.board.join('').replaceAll(',', '')
+    return this.#board.join('').replaceAll(',', '')
   }
 
   drop(block) {
     // * Multiple blocks will be falling during the turn but there can be only one block falling at a time
     if (!this.hasFalling()) {
       this.block = block
-      this.board[0][1] = this.block.getColor()
+      this.#board[0][1] = this.block.getColor()
     }
     else {
       throw new Error('already falling')
@@ -83,12 +84,12 @@ export class Board {
   }
 
   moveBlock(y, x, previousPositionY) {
-    if (y >= this.height) {
+    if (y >= this.#height) {
       this.block = null
     }
     else if (this.isEmptyBoardSquare(y, x)) {
-      this.board[previousPositionY][x] = this.boardSquare
-      this.board[y][x] = this.block.getColor()
+      this.#board[previousPositionY][x] = this.#boardSquare
+      this.#board[y][x] = this.block.getColor()
     }
     else {
       this.block = null
@@ -100,10 +101,10 @@ export class Board {
   }
 
   boardIsCreated() {
-    return !!this.board.length
+    return !!this.#board.length
   }
 
   isEmptyBoardSquare(y, x) {
-    return this.board[y][x] === this.boardSquare
+    return this.#board[y][x] === this.#boardSquare
   }
 }
--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -6,10 +6,26 @@ export class RotatingShape {
   }
 
   toString() {
+    console.log('shape', this.shape.replaceAll(' ', '') + '\n');
     return this.shape.replaceAll(' ', '') + '\n'
   }
 
   rotateRight() {
+    // y = 0 then y = x and x = 2
+    // y = 1 then y = x and x = 1
+    // y = 2 then y = x and x = 0
+
+    // shape[0][0] = shape[0][2]
+    // shape[0][1] = shape[1][2]
+    // shape[0][2] = shape[2][2]
+
+    // shape[1][0] = shape[0][1]
+    // shape[1][1] = shape[1][1]
+    // shape[1][2] = shape[2][1]
+
+    // shape[2][0] = shape[0][0]
+    // shape[2][1] = shape[1][0]
+    // shape[2][2] = shape[2][0]
     this.shape =
       `GDA
        HEB
