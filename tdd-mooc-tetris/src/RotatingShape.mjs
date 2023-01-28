e48f3e70ed6d1c62683a502c1063d2cd0c8aaab4
pablo-maff <pablo.maff.076@gmail.com>
2023-01-28 12:56:08 +0100

    level 2 improved

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -4,54 +4,38 @@ export class RotatingShape {
   // * filter and clean data here. make shape a 2D array, it will be easier to rotate
   constructor(shape) {
     this.shape = shape
+    this.cleanShape = shape.split(' ').filter(shapeBit =>
+      shapeBit !== '')
   }
-  #rotate = 0;
 
   toString() {
-    // * Will be a single line after implementing rotateRight and rotateLeft properly
-    if (!this.#rotate) return this.shape.replaceAll(' ', '') + '\n'
-
-    return this.rotateShape().join().replaceAll(',', '')
+    return this.cleanShape.join().replaceAll(',', '').replaceAll(' ', '') + '\n'
   }
 
-  // ! Time to make this proper
-  rotateShape = () => {
-    const shape = this.shape.split('').filter(shapeBit => shapeBit !== ' ' && shapeBit !== '\n')
-    const shapeWidthAndHeight = Math.sqrt(this.shape.replaceAll(' ', '').replaceAll('\n', '').length);
-
-    let newShape = []
-    let colNum = 0
-
-    for (let y = 0; y < shapeWidthAndHeight; y++) {
-      newShape[y] = []
-      for (let x = 0; x < shapeWidthAndHeight; x++) {
-        newShape[y].push(shape[shapeWidthAndHeight * x + colNum])
-      }
-      // * It would be better to separate them into two different functions
-      // * rotates right
-      if (this.#rotate === 1) {
-        newShape[y].reverse()
-      }
-      newShape[y].push('\n')
-      colNum++
-    }
-    // * rotates left
-    if (this.#rotate === 3) {
-      newShape.reverse()
-    }
-    return newShape
-  }
-
-  // * return a new rotateShape instance from this methods
   rotateRight() {
-    this.#rotate = 1
+    this.result = []
+    const shapeLength = this.cleanShape.at(-1).length
+
+    for (let y = 0; y < shapeLength; y++) {
+      this.result[y] = []
+      for (let x = 0; x < shapeLength; x++) {
+        this.result[y][x] = this.cleanShape[shapeLength - x - 1][y]
+      }
+      if (y !== shapeLength - 1) {
+        this.result[y].push('\n')
+      }
+    }
+    this.cleanShape = [...this.result]
 
     return this
   }
 
   rotateLeft() {
-    this.#rotate = 3
+    const rotateShape = new RotatingShape(this.shape)
+    rotateShape.rotateRight()
+    rotateShape.rotateRight()
+    rotateShape.rotateRight()
 
-    return this
+    return rotateShape
   }
 }
