70479e1d4fd48252d73ba4b3080381fe397313e6
pablo-maff <pablo.maff.076@gmail.com>
2023-02-01 00:40:03 +0100

    level 1 refactored

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -27,17 +27,16 @@ export class Board {
   }
 
   toString() {
-    const { currentBlockRow, currentBlockCol } = this.getCurrentBlockPosition()
+    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
     let result = ''
     for (let row = 0; row < this.#height; row++) {
       for (let col = 0; col < this.#width; col++) {
-        // TODO: Works for #2 but breaks the rest
-        // if (row === currentBlockRow && col === currentBlockCol) {
-        //   result += this.block.getColor();
-        // }
-        // else {
-        result += this.board[row][col];
-        // }
+        if (row === currentBlockRow && col === currentBlockCol) {
+          result += this.block.getColor();
+        }
+        else {
+          result += this.board[row][col];
+        }
       }
       result += '\n';
     }
@@ -47,57 +46,52 @@ export class Board {
   drop(block) {
     if (this.hasFalling()) throw new Error("already falling")
     this.block = block;
-    this.setCurrentBlockPosition(0, 1)
-    this.board[0][1] = this.block.getColor()
+    this.#setCurrentBlockPosition(0, 1)
   }
 
   tick() {
-    const { currentBlockRow, currentBlockCol } = this.getCurrentBlockPosition()
+    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
 
     if (currentBlockRow < this.#height - 1) {
-      this.moveBlock(currentBlockRow + 1, currentBlockCol)
+      this.#moveBlock(currentBlockRow + 1, currentBlockCol)
     }
     else {
-      this.setStationaryBlockInBoard(currentBlockRow, currentBlockCol)
+      this.#stopBlockMovement()
     }
-  }
-
-  moveBlock(row, col) {
-    if (this.isEmptyBoardSquare(row, col)) {
-      this.setCurrentBlockPosition(row, col)
-      this.board[row - 1][col] = this.#boardSquare;
-      this.board[row][col] = this.block.getColor();
-    }
-    else {
-      this.setStationaryBlockInBoard(row, col)
-    };
-  };
-
-  setCurrentBlockPosition(row, col) {
-    this.#currentRow = row;
-    this.#currentCol = col;
-  };
-
-  getCurrentBlockPosition() {
-    return { currentBlockRow: this.#currentRow, currentBlockCol: this.#currentCol };
-  };
-
-  setStationaryBlockInBoard(row, col) {
-    this.board2[row][col] = this.block.getColor();
-    this.stopBlockMovement()
-  }
-
-  stopBlockMovement() {
-    this.block = null;
-    this.#currentRow = null;
-    this.#currentCol = null
   }
 
   hasFalling() {
     return !!this.block
   }
 
-  isEmptyBoardSquare(row, col) {
+  #moveBlock(row, col) {
+    if (this.#isEmptyBoardSquare(row, col)) {
+      this.#setCurrentBlockPosition(row, col)
+    }
+    else {
+      this.#stopBlockMovement()
+    };
+  };
+
+  #setCurrentBlockPosition(row, col) {
+    this.#currentRow = row;
+    this.#currentCol = col;
+  };
+
+  #getCurrentBlockPosition() {
+    return { currentBlockRow: this.#currentRow, currentBlockCol: this.#currentCol };
+  };
+
+  #stopBlockMovement() {
+    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
+
+    this.board[currentBlockRow][currentBlockCol] = this.block.getColor();
+
+    this.#setCurrentBlockPosition(null, null)
+    this.block = null;
+  }
+
+  #isEmptyBoardSquare(row, col) {
     return this.board[row][col] === this.#boardSquare;
   }
 }
