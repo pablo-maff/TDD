91b4327b0863def75ae6d8357fd44c212560c053
pablo-maff <pablo.maff.076@gmail.com>
2023-02-03 22:34:11 +0100

    refactored currentCol

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -27,13 +27,12 @@ export class Board {
   }
 
   toString() {
-    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
+    const { currentBlockRow, currentBlockCol2 } = this.#getCurrentBlockPosition()
     let result = ''
     let shapeIndex = 0
     for (let row = 0; row < this.#height; row++) {
       for (let col = 0; col < this.#width; col++) {
-        if (row >= currentBlockRow?.start && row <= currentBlockRow?.end && col === currentBlockCol) {
-          console.log('this.block.getShape()', this.block.getShape());
+        if (row >= currentBlockRow?.start && row <= currentBlockRow?.end && col >= currentBlockCol2?.start && col <= currentBlockCol2?.end) {
           result += this.block.getShape();
         }
         else {
@@ -53,10 +52,10 @@ export class Board {
   }
 
   tick() {
-    const { currentBlockCol, currentBlockRow } = this.#getCurrentBlockPosition()
+    const { currentBlockRow, currentBlockCol2 } = this.#getCurrentBlockPosition()
 
     if (currentBlockRow.end < this.#height - 1) {
-      this.#moveBlock(currentBlockRow.end, currentBlockCol)
+      this.#moveBlock(currentBlockRow.end, currentBlockCol2.end)
     }
     else {
       this.#stopBlockMovement()
@@ -69,7 +68,7 @@ export class Board {
 
   #moveBlock(row, col) {
     if (this.#isEmptyBoardSquare(row, col)) {
-      this.#setBlockCurrentPosition(col)
+      this.#setBlockCurrentPosition()
     }
     else {
       this.#stopBlockMovement()
@@ -79,30 +78,39 @@ export class Board {
   #setBlockInitialPosition(tetrominoLength) {
     const boardMiddleCol = Math.round((this.#width / 2) - 1)
     const tetrominoOffset = Math.round(tetrominoLength / 2 - 1)
+    const startingCol = tetrominoLength > 0 ? boardMiddleCol - tetrominoOffset : 1
 
     this.#currentRow = { start: 0, end: tetrominoLength }
     this.#currentCol = tetrominoLength > 0 ? boardMiddleCol - tetrominoOffset : 1
+    this.#currentCol = {
+      start: startingCol,
+      end: startingCol + tetrominoLength
+    }
   }
 
-  #setBlockCurrentPosition(col, empty) {
+  #setBlockCurrentPosition(empty) {
     this.#currentRow = !empty ? {
       start: this.#currentRow.start + 1,
       end: this.#currentRow.end + 1
     } : null
 
-    this.#currentCol = col;
+    this.#currentCol = !empty ? {
+      start: this.#currentCol.start, // * For now the piece can't move to the sides
+      end: this.#currentCol.end
+    } : null
+
   };
 
   #getCurrentBlockPosition() {
-    return { currentBlockRow: this.#currentRow, currentBlockCol: this.#currentCol };
+    return { currentBlockRow: this.#currentRow, currentBlockCol2: this.#currentCol };
   };
 
   #stopBlockMovement() {
-    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
+    const { currentBlockRow, currentBlockCol2 } = this.#getCurrentBlockPosition()
 
-    this.board[currentBlockRow.start][currentBlockCol] = this.block.getShape();
+    this.board[currentBlockRow.start][currentBlockCol2.start] = this.block.getShape();
 
-    this.#setBlockCurrentPosition(null, true)
+    this.#setBlockCurrentPosition(true)
     this.block = null;
   }
 
