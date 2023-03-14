2f3e5708d809dd8dea6fa2f46e0d9262ea50c163
pablo-maff <pablo.maff.076@gmail.com>
2023-03-14 22:13:43 +0100

    Board refactored

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -11,8 +11,6 @@ export class Board {
   }
 
   #boardSquare = '.';
-  #currentRow;
-  #currentCol;
   #boardCurrentTopRow = 0;
   #blocksOnBoard = [];
 
@@ -23,65 +21,47 @@ export class Board {
       for (let col = 0; col < this.#width; col++) {
         emptyBoard[row][col] = this.#boardSquare;
       }
-      // emptyBoard[row][this.#width] = '\n';
     }
     return emptyBoard
   }
 
   toString() {
-    // TODO: Make it work by asking or getting the current position of the shape (not the block)
-
-    // * Print blocksOnBoard and if there is a currentBlock, print it as well
-    let result2 = ''
+    let result = ''
 
     const blocksInPlace = this.#blocksOnBoard
-    const currentBlock = this.getBlockCurrentPosition2(this.#boardCurrentTopRow, this.#boardMiddleCol)
-    console.log('#blocksOnBoard', this.#blocksOnBoard)
-    // console.log('currentBlock', this.getBlockCurrentPosition2(this.#boardCurrentTopRow, this.#boardMiddleCol))
+    const currentBlock = this.getBlockCurrentPosition(this.#boardCurrentTopRow, this.#boardMiddleCol)
 
     for (let row = 0; row < this.#height; row++) {
       for (let col = 0; col < this.#width; col++) {
         const currentBlockInBoard = currentBlock?.find(block => block.row === row && block.column === col)
-
-        // ! TODO NEXT: print in the board the blocks that stopped moving in stopBlockMovement2
         const allBlocksInBoard = blocksInPlace?.find(block => block.row === row && block.column === col)
-        if (currentBlockInBoard || allBlocksInBoard) {
-          result2 += this.block.getShape2();
+
+        if (this.block && (currentBlockInBoard || allBlocksInBoard)) {
+          result += this.block.getShape2();
         }
         else {
-          result2 += this.board[row][col];
+          result += this.board[row][col];
         }
       }
-      result2 += '\n';
+      result += '\n';
     }
 
-    console.log('result2', result2);
-
-    return result2
+    return result
   }
 
   drop(block) {
     if (this.hasFalling()) throw new Error("already falling")
+
     this.block = block;
-
-    // this.#setBlockCurrentPosition()
-
-    // !Deprecated
-    // this.#setBlockInitialPosition(this.block.getLength())
   }
 
   tick() {
-    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
-
-    // TODO: new implementation of isEmptyBoardSquare
-
-    if (this.#isEmptyBoardSquare2(this.#boardCurrentTopRow + 1, this.#boardMiddleCol)) {
+    if (this.#isEmptyBoardSquare(this.#boardCurrentTopRow + 1, this.#boardMiddleCol)) {
       this.#moveBlock()
     }
 
     else if (this.hasFalling()) {
-      this.#stopBlockMovement2(this.#boardCurrentTopRow, this.#boardMiddleCol)
-      // this.#stopBlockMovement()
+      this.#stopBlockMovement(this.#boardCurrentTopRow, this.#boardMiddleCol)
     }
   }
 
@@ -93,101 +73,58 @@ export class Board {
     this.#setBlockCurrentPosition()
   };
 
-  // ? Check positions in board instead of tetrominolength
-  #setBlockInitialPosition(tetrominoLength) {
-    const boardMiddleCol = Math.round((this.#width / 2) - 1)
-    const tetrominoOffset = Math.round(tetrominoLength / 2 - 1)
-    const startingCol = tetrominoLength > 0 ? boardMiddleCol - tetrominoOffset : 1
-
-    this.#currentRow = { start: 0, end: tetrominoLength }
-    this.#currentCol = tetrominoLength > 0 ? boardMiddleCol - tetrominoOffset : 1
-    this.#currentCol = {
-      start: startingCol,
-      end: startingCol + tetrominoLength
-    }
-  }
-
-  getBlockCurrentPosition2(row, col) {
+  getBlockCurrentPosition(row, col) {
     if (!this.hasFalling()) return null
 
     const currentPosition = this.block.mapToBoardCoordinates(row, col)
 
-    // console.log('currentPosition', currentPosition);
-
     return currentPosition
   }
 
-  #setBlockCurrentPosition(empty) {
-    // console.log('this.#boardCurrentTopRow', this.#boardCurrentTopRow);
-    this.#boardCurrentTopRow = !empty ? this.#boardCurrentTopRow + 1 : null
-    // this.#currentRow = !empty ? {
-    //   start: this.#currentRow.start + 1,
-    //   end: this.#currentRow.end + 1
-    // } : null
-
-    // this.#currentCol = !empty ? {
-    //   start: this.#currentCol.start, // * For now the piece can't move to the sides
-    //   end: this.#currentCol.end
-    // } : null
-
+  #setBlockCurrentPosition() {
+    this.#boardCurrentTopRow += 1
   };
 
-  #getCurrentBlockPosition() {
-    return { currentBlockRow: this.#currentRow, currentBlockCol: this.#currentCol };
-  };
-
-  #stopBlockMovement2(row, col) {
-    const currentBlockPosition = this.getBlockCurrentPosition2(row, col)
-
-    // console.log('this.board', this.board);
+  #stopBlockMovement(row, col) {
+    const currentBlockPosition = this.getBlockCurrentPosition(row, col)
 
     this.#blocksOnBoard = [...this.#blocksOnBoard, ...currentBlockPosition]
 
-    // ! TODO NEXT: Fix toString
-
-    this.block = null
-  }
-
-  // ? ask the board if there is something at coordinate x,y
-  #stopBlockMovement() {
-    const { currentBlockRow, currentBlockCol } = this.#getCurrentBlockPosition()
-    // TODO: Make it work by asking or getting the current position of the shape (not the block)
-
     let result = []
-    let shapeIndex = 0
+
+    const blocksInPlace = this.#blocksOnBoard
+    const currentBlock = this.getBlockCurrentPosition(this.#boardCurrentTopRow, this.#boardMiddleCol)
+
     for (let row = 0; row < this.#height; row++) {
       result[row] = []
       for (let col = 0; col < this.#width; col++) {
-        // ! NEXT TODO: Refactor
-        if (this.block?.getShape()[shapeIndex] === '\n') shapeIndex++
-        if (row >= currentBlockRow?.start && row <= currentBlockRow?.end && col >= currentBlockCol?.start && col < currentBlockCol?.end) {
-          if (this.block?.getShape()[shapeIndex] !== undefined) {
-            result[row][col] = this.block.getShape()[shapeIndex];
-            shapeIndex++
-          }
-          else {
-            result[row][col] = this.board[row][col];
-          }
+        const currentBlockInBoard = currentBlock?.find(block => block.row === row && block.column === col)
+        const allBlocksInBoard = blocksInPlace?.find(block => block.row === row && block.column === col)
+
+        if (currentBlockInBoard || allBlocksInBoard) {
+          result[row][col] = this.block.getShape2();
         }
         else {
           result[row][col] = this.board[row][col];
         }
       }
-      result[row].push('\n');
     }
 
     this.board = result
 
-    this.block = null;
+    this.#boardCurrentTopRow = 0
+
+    this.block = null
   }
 
-  #isEmptyBoardSquare2(row, col) {
+  #isEmptyBoardSquare(row, col) {
     if (!this.hasFalling()) return false
-    const nextBlockPosition = this.getBlockCurrentPosition2(row, col)
+    const nextBlockPosition = this.getBlockCurrentPosition(row, col)
 
     const willColide = nextBlockPosition.reduce((collision, nextPosition) => {
       if (nextPosition.row >= this.#height || nextPosition.column >= this.#width) {
         collision = true
+        return collision
       }
       this.#blocksOnBoard.forEach(blockPosition => {
         if (nextPosition.row === blockPosition.row && nextPosition.column === blockPosition.column) {
@@ -199,23 +136,6 @@ export class Board {
 
     if (willColide) return false
 
-    return true
-  }
-
-  #isEmptyBoardSquare(row, col) {
-    // ! Only works for 3x3 blocks with T shape
-    // TODO: Make it work by asking or getting the current position of the shape (not the block)
-    const blockLastRow = this.block?.getShape().substring(this.block?.getShape().length - 4, this.block?.getShape().length - 1)
-
-    if (row > this.#height) return false
-    else if (this.board[row]?.includes('T') && [blockLastRow].includes('T')) {
-      return false
-    }
-    else if (this.board[row]?.includes('T') && ![blockLastRow].includes('T')) {
-      this.block?.trimEnd()
-      this.#setBlockCurrentPosition()
-      return false
-    }
     return true
   }
 }
