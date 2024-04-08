3f50668e02815bf26143d4a5606ded3bcbce13ef
pablo-maff <pablo.maff.076@gmail.com>
2024-04-08 11:21:22 +0200

    level 4

--- tdd-mooc-tetris/test/FallingTetrominoes.test.ts
+++ tdd-mooc-tetris/test/FallingTetrominoes.test.ts
@@ -1,60 +1,60 @@
-// import { beforeEach, describe, test } from "vitest";
-// import { expect } from "chai";
-// import { Board } from "../src/Board.js";
-// import { Tetromino } from "../src/Tetromino.js";
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.js";
+import { Tetromino } from "../src/Tetromino.js";
 
-// function fallToBottom(board) {
-//   for (let i = 0; i < 10; i++) {
-//     board.tick();
-//   }
-// }
+function fallToBottom(board: Board) {
+  for (let i = 0; i < 10; i++) {
+    board.tick();
+  }
+}
 
-// describe("Falling tetrominoes", () => {
-//   let board;
-//   beforeEach(() => {
-//     board = new Board(10, 6);
-//   });
+describe("Falling tetrominoes", () => {
+  let board: Board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
 
-//   test("start from the top middle", () => {
-//     board.drop(Tetromino.T_SHAPE);
+  test("start from the top middle", () => {
+    board.drop(Tetromino.T_SHAPE);
 
-//     expect(board.toString()).to.equalShape(
-//       `....T.....
-//        ...TTT....
-//        ..........
-//        ..........
-//        ..........
-//        ..........`
-//     );
-//   });
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ...TTT....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
 
-//   test.skip("stop when they hit the bottom", () => {
-//     board.drop(Tetromino.T_SHAPE);
-//     fallToBottom(board);
+  test.skip("stop when they hit the bottom", () => {
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
 
-//     expect(board.toString()).to.equalShape(
-//       `..........
-//        ..........
-//        ..........
-//        ..........
-//        ....T.....
-//        ...TTT....`
-//     );
-//   });
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ....T.....
+       ...TTT....`
+    );
+  });
 
-//   test.skip("stop when they land on another block", () => {
-//     board.drop(Tetromino.T_SHAPE);
-//     fallToBottom(board);
-//     board.drop(Tetromino.T_SHAPE);
-//     fallToBottom(board);
+  test.skip("stop when they land on another block", () => {
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
 
-//     expect(board.toString()).to.equalShape(
-//       `..........
-//        ..........
-//        ....T.....
-//        ...TTT....
-//        ....T.....
-//        ...TTT....`
-//     );
-//   });
-// });
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ....T.....
+       ...TTT....
+       ....T.....
+       ...TTT....`
+    );
+  });
+});
