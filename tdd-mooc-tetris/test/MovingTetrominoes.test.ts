f551699ae9e4d9e1c39fd20d3d213fe208d456c7
pablo-maff <pablo.maff.076@gmail.com>
2024-04-10 16:57:35 +0200

    cannot be moved down through other blocks and block stops falling

--- tdd-mooc-tetris/test/MovingTetrominoes.test.ts
+++ tdd-mooc-tetris/test/MovingTetrominoes.test.ts
@@ -10,399 +10,180 @@ describe("Moving tetrominoes", () => {
     board = new Board(10, 6);
   });
 
-  describe("The T shape", () => {
-    test("can be moved left", () => {
-      board.drop(Tetromino.T_SHAPE);
-      board.moveBlockLeft();
+  test("can be moved left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveBlockLeft();
 
-      expect(board.toString()).to.equalShape(
-        `...T......
+    expect(board.toString()).to.equalShape(
+      `...T......
          ..TTT.....
          ..........
          ..........
          ..........
          ..........`
-      );
-    });
+    );
+  });
 
-    test("can be moved right", () => {
-      board.drop(Tetromino.T_SHAPE);
-      board.moveBlockRight();
+  test("can be moved right", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveBlockRight();
 
-      expect(board.toString()).to.equalShape(
-        `.....T....
+    expect(board.toString()).to.equalShape(
+      `.....T....
          ....TTT...
          ..........
          ..........
          ..........
          ..........`
-      );
-    });
+    );
+  });
 
-    test("can be moved down", () => {
-      board.drop(Tetromino.T_SHAPE);
-      board.tick();
+  test("can be moved down", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
 
-      expect(board.toString()).to.equalShape(
-        `..........
+    expect(board.toString()).to.equalShape(
+      `..........
          ....T.....
          ...TTT....
          ..........
          ..........
          ..........`
-      );
-    });
+    );
+  });
 
-    test("cannot be moved left if it hits wall", () => {
-      board.drop(Tetromino.T_SHAPE);
-      moveLeft(board, 10);
+  test("cannot be moved left if it hits wall", () => {
+    board.drop(Tetromino.T_SHAPE);
+    moveLeft(board, 10);
 
-      expect(board.toString()).to.equalShape(
-        `.T........
+    expect(board.toString()).to.equalShape(
+      `.T........
          TTT.......
          ..........
          ..........
          ..........
          ..........`
-      );
-    });
+    );
+  });
 
-    test("cannot be moved right if it hits wall", () => {
-      board.drop(Tetromino.T_SHAPE);
-      moveRight(board, 10);
+  test("cannot be moved right if it hits wall", () => {
+    board.drop(Tetromino.T_SHAPE);
+    moveRight(board, 10);
 
-      expect(board.toString()).to.equalShape(
-        `........T.
+    expect(board.toString()).to.equalShape(
+      `........T.
          .......TTT
          ..........
          ..........
          ..........
          ..........`
-      );
-    });
+    );
+  });
 
-    test("cannot be moved down beyond the board and stops falling", () => {
-      board.drop(Tetromino.T_SHAPE);
-      fallToBottom(board);
+  test("cannot be moved down beyond the board and stops falling", () => {
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
 
-      expect(board.toString()).to.equalShape(
-        `..........
+    expect(board.toString()).to.equalShape(
+      `..........
          ..........
          ..........
          ..........
          ....T.....
          ...TTT....`
-      );
+    );
 
-      expect(board.hasFalling(), "the player should not be able to move the block").to.be.false;
-    });
+    expect(board.hasFalling(), "the player should not be able to move the block").to.be.false;
+  });
 
-    test("still moves when reaches the bottom", () => {
-      board.drop(Tetromino.T_SHAPE);
-      board.tick();
-      board.tick();
-      board.tick();
-      board.tick();
+  test("still moves when reaches the bottom", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
+    board.tick();
 
-      expect(board.toString()).to.equalShape(
-        `..........
+    expect(board.toString()).to.equalShape(
+      `..........
          ..........
          ..........
          ..........
          ....T.....
          ...TTT....`
-      );
+    );
 
-      expect(board.hasFalling(), "the player should still be able to move the block").to.be.true;
-    });
+    expect(board.hasFalling(), "the player should still be able to move the block").to.be.true;
+  });
 
-    test("cannot be moved left through other blocks", () => {
-      board.drop(Tetromino.T_SHAPE);
-      moveLeft(board, 5);
-      fallToBottom(board);
+  test("cannot be moved left through other blocks", () => {
+    board.drop(Tetromino.T_SHAPE);
+    moveLeft(board, 5);
+    fallToBottom(board);
 
-      board.drop(Tetromino.T_SHAPE);
-      board.tick();
-      board.tick();
-      board.tick();
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
 
-      moveLeft(board, 10);
+    moveLeft(board, 10);
 
-      expect(board.toString()).to.equalShape(
-        `..........
+    expect(board.toString()).to.equalShape(
+      `..........
          ..........
          ..........
          ...T......
          .TTTT.....
          TTT.......`
-      );
-    });
+    );
+  });
 
-    test("cannot be moved right through other blocks", () => {
-      board.drop(Tetromino.T_SHAPE);
-      moveRight(board, 5);
-      fallToBottom(board);
+  test("cannot be moved right through other blocks", () => {
+    board.drop(Tetromino.T_SHAPE);
+    moveRight(board, 5);
+    fallToBottom(board);
 
-      board.drop(Tetromino.T_SHAPE);
-      board.tick();
-      board.tick();
-      board.tick();
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
 
-      moveRight(board, 10);
+    moveRight(board, 10);
 
-      expect(board.toString()).to.equalShape(
-        `..........
+    expect(board.toString()).to.equalShape(
+      `..........
          ..........
          ..........
          ......T...
          .....TTTT.
          .......TTT`
-      );
+    );
 
-      expect(board.hasFalling(), "the player should be able to move the block").to.be.true;
-    });
-
-    // test("cannot be moved down through other blocks and block stops falling", () => {
-    //   board.drop(Tetromino.T_SHAPE);
-    //   moveLeft(board, 5)
-    //   fallToBottom(board)
-
-    //   board.drop(Tetromino.T_SHAPE);
-    //   board.tick()
-    //   board.tick()
-    //   board.tick()
-
-    //   moveLeft(board, 10)
-
-    //   board.tick()
-
-    //   expect(board.toString()).to.equalShape(
-    //     `..........
-    //      ..........
-    //      ..........
-    //      ..TTT.....
-    //      TTTT......
-    //      .T........`
-    //   );
-
-    //   expect(
-    //     board.hasFalling(),
-    //     "the player should not be able to move the block"
-    //   ).to.be.false;
-    // })
+    expect(board.hasFalling(), "the player should be able to move the block").to.be.true;
   });
 
-  /*
-  describe("The I shape", () => {
-    test("can be moved left", () => {
-      board.drop(Tetromino.I_SHAPE);
-      board.moveBlockLeft()
+  test("cannot be moved down through other blocks and block stops falling", () => {
+    board.drop(Tetromino.T_SHAPE);
+    moveLeft(board, 5);
+    fallToBottom(board);
 
-      expect(board.toString()).to.equalShape(
-        `..IIII....
-         ..........
-         ..........
-         ..........
-         ..........
-         ..........`
-      );
-    });
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
 
-    test("can be moved right", () => {
-      board.drop(Tetromino.I_SHAPE);
-      board.moveBlockRight()
+    moveLeft(board, 10);
 
-      expect(board.toString()).to.equalShape(
-        `....IIII..
-         ..........
-         ..........
-         ..........
-         ..........
-         ..........`
-      );
-    });
+    board.tick();
 
-    test("can be moved down", () => {
-      board.drop(Tetromino.I_SHAPE);
-      board.moveBlockDown()
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ...IIII...
+    expect(board.toString()).to.equalShape(
+      `..........
          ..........
          ..........
-         ..........
-         ..........`
-      );
-    });
+         ...T......
+         .TTTT.....
+         TTT.......`
+    );
 
-    test("cannot be moved left beyond the board and is still falling", () => {
-      board.drop(Tetromino.I_SHAPE);
-      moveLeft(board, 10)
-
-      expect(board.toString()).to.equalShape(
-        `IIII......
-         ..........
-         ..........
-         ..........
-         ..........
-         ..........`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should still be able to move the block"
-      ).to.be.true;
-    })
-
-    test("cannot be moved right beyond the board and is still falling", () => {
-      board.drop(Tetromino.I_SHAPE);
-      moveRight(board, 10)
-
-      expect(board.toString()).to.equalShape(
-        `......IIII
-         ..........
-         ..........
-         ..........
-         ..........
-         ..........`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should still be able to move the block"
-      ).to.be.true;
-    })
-
-    test("cannot be moved down beyond the board and stops falling", () => {
-      board.drop(Tetromino.I_SHAPE);
-      fallToBottom(board)
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ..........
-         ..........
-         ..........
-         ..........
-         ...IIII...`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should not be able to move the block"
-      ).to.be.false;
-    })
-
-    test("still moves when reaches the bottom", () => {
-      board.drop(Tetromino.I_SHAPE);
-      board.tick()
-      board.tick()
-      board.tick()
-      board.tick()
-      board.tick()
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ..........
-         ..........
-         ..........
-         ..........
-         ...IIII...`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should still be able to move the block"
-      ).to.be.true;
-    })
-
-    test("cannot be moved left through other blocks", () => {
-      board.drop(Tetromino.I_SHAPE);
-      moveLeft(board, 5)
-      fallToBottom(board)
-
-      board.drop(Tetromino.I_SHAPE);
-      moveLeft(board, 5)
-      fallToBottom(board)
-
-      board.drop(Tetromino.I_SHAPE);
-      board.moveBlockRight()
-      board.tick()
-      board.tick()
-      board.tick()
-      board.tick()
-
-      moveLeft(board, 10)
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ..........
-         ..........
-         ..........
-         IIIIIIII..
-         IIII......`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should be able to move the block"
-      ).to.be.true;
-    })
-
-    test("cannot be moved right through other blocks", () => {
-      board.drop(Tetromino.I_SHAPE);
-      moveRight(board, 5)
-      fallToBottom(board)
-
-      board.drop(Tetromino.I_SHAPE);
-      moveRight(board, 5)
-      fallToBottom(board)
-
-      board.drop(Tetromino.I_SHAPE);
-      board.moveBlockLeft()
-      board.tick()
-      board.tick()
-      board.tick()
-      board.tick()
-
-      moveRight(board, 10)
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ..........
-         ..........
-         ..........
-         ..IIIIIIII
-         ......IIII`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should be able to move the block"
-      ).to.be.true;
-    })
-    test("cannot be moved down through other blocks and block stops falling", () => {
-      board.drop(Tetromino.I_SHAPE);
-      fallToBottom(board)
-
-      board.drop(Tetromino.I_SHAPE);
-      fallToBottom(board)
-      board.moveBlockDown()
-
-      expect(board.toString()).to.equalShape(
-        `..........
-         ..........
-         ..........
-         ..........
-         ...IIII...
-         ...IIII...`
-      );
-
-      expect(
-        board.hasFalling(),
-        "the player should not be able to move the block"
-      ).to.be.false;
-    })
-  })
-  */
+    expect(board.hasFalling(), "the player should not be able to move the block").to.be.false;
+  });
 });
