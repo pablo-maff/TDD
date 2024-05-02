b52990531aeafa8370204213e1f59f95b0d88093
pablo-maff <pablo.maff.076@gmail.com>
2024-05-02 22:04:28 +0200

    nintendo scoring system adds 40 points if 1 line is cleared

--- tdd-mooc-tetris/src/NintendoScoring.ts
+++ tdd-mooc-tetris/src/NintendoScoring.ts
@@ -3,8 +3,8 @@ import { Observer } from "./Observer";
 export class NintendoScoring implements Observer {
   #state: number = 0;
 
-  update(data: number): void {
-    return;
+  update(linesCleared: number): void {
+    this.#state = 40;
   }
 
   public get value(): number {
--- tdd-mooc-tetris/test/NintentdoScoring.test.ts
+++ tdd-mooc-tetris/test/NintentdoScoring.test.ts
@@ -1,11 +1,37 @@
 import { describe, test } from "vitest";
 import { NintendoScoring } from "../src/NintendoScoring";
 import { expect } from "chai";
+import { Board } from "../src/Board";
+import { Tetromino } from "../src/Tetromino";
 
 describe("Nintento Scoring system", () => {
   test("starts with 0 points", () => {
     const scoring = new NintendoScoring();
 
     expect(scoring.value).to.equal(0);
+  });
+
+  test("adds 40 points if 1 line is cleared", () => {
+    const board = Board.loadBoard(
+      `.........
+       .........
+       .........
+       .........
+       XXXX.XXXX
+       XXXX.XXXX`
+    );
+
+    const scoring = new NintendoScoring();
+
+    board.events.subscribe(scoring);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
+    board.tick();
+    board.tick();
+
+    expect(scoring.value).to.equal(40);
   });
 });
