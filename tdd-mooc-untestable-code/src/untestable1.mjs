ad0dc88d0ddfb3a03737e1a7fb3fc86a798d37b9
pablo-maff <pablo.maff.076@gmail.com>
2024-05-07 12:30:10 +0200

    refactored daysUntilChristmas

--- tdd-mooc-untestable-code/src/untestable1.mjs
+++ tdd-mooc-untestable-code/src/untestable1.mjs
@@ -3,12 +3,11 @@ const millisPerDay = 24 * 60 * 60 * 1000;
 // * Reading the current time is untestable.
 // TODO: 1. Pass now as the function parameter
 // TODO: 2. Replace new Date passed as argument of christmasDay.setFullYear with the parameter now
-export function daysUntilChristmas() {
-  const now = new Date();
+export function daysUntilChristmas(now) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
-    christmasDay.setFullYear(new Date().getFullYear() + 1);
+    christmasDay.setFullYear(now.getFullYear() + 1);
   }
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -3,8 +3,10 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
+  const now = new Date()
+
   test("todo", () => {
     // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+    expect(daysUntilChristmas(now)).to.be.a("number");
   });
 });
