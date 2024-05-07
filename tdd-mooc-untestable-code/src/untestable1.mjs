24fa1771ce22eb7ee43da7a6c129ee91ec17a174
pablo-maff <pablo.maff.076@gmail.com>
2024-05-07 12:44:51 +0200

    untestable1 done

--- tdd-mooc-untestable-code/src/untestable1.mjs
+++ tdd-mooc-untestable-code/src/untestable1.mjs
@@ -1,8 +1,8 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
 // * Reading the current time is untestable.
-// TODO: 1. Pass now as the function parameter
-// TODO: 2. Replace new Date passed as argument of christmasDay.setFullYear with the parameter now
+// TODO: 1. Pass now as the function parameter DONE
+// TODO: 2. Replace new Date passed as argument of christmasDay.setFullYear with the parameter now DONE
 export function daysUntilChristmas(now) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -3,10 +3,27 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  const now = new Date()
+  const now = new Date('2022-12-20T03:24:00')
 
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas(now)).to.be.a("number");
-  });
+  test("returns correct remaining days if now is on the same year before christmas", () => {
+    expect(daysUntilChristmas(now)).to.equal(5)
+  })
+
+  test("returns correct remaining days if now is on the same year after christmas", () => {
+    const now = new Date('2022-12-26T03:24:00')
+
+    expect(daysUntilChristmas(now)).to.equal(364)
+  })
+
+  test("returns correct remaining days if now is on christmas", () => {
+    const now = new Date('2022-12-25T03:24:00')
+
+    expect(daysUntilChristmas(now)).to.equal(0)
+  })
+
+  test("returns correct remaining days if there is a leap year", () => {
+    const now = new Date('2023-12-26T03:24:00')
+
+    expect(daysUntilChristmas(now)).to.equal(365)
+  })
 });
