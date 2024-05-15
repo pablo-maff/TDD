8f32f0c4ee0ee0baafba65552a0a7859e57a491c
pablo-maff <pablo.maff.076@gmail.com>
2024-05-15 23:17:39 +0200

    untangled rest of loop

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -35,32 +35,27 @@ export class Shop {
     for (var i = 0; i < this.items.length; i++) {
       const currentItem = this.items[i]
 
-      if (!this.#isAgedBrie(currentItem.name) && !this.#isBackstagePass(currentItem.name)) {
-        if (currentItem.quality > 0) {
-          if (!this.#isSulfuras(currentItem.name)) {
-            this.#updateQuality(i, -1)
-          }
-        }
-      } else {
+      if (this.#isAgedBrie(currentItem.name) || this.#isBackstagePass(currentItem.name)) {
         if (currentItem.quality < 50) {
           this.#updateQuality(i, 1)
-          if (this.#isBackstagePass(currentItem.name)) {
-            if (currentItem.sellIn < 11) {
-              if (currentItem.quality < 50) {
-                this.#updateQuality(i, 1)
-              }
-            }
+          if (this.#isBackstagePass(currentItem.name) && currentItem.sellIn < 11 && currentItem.quality < 50) {
             if (currentItem.sellIn < 6) {
-              if (currentItem.quality < 50) {
-                this.#updateQuality(i, 1)
-              }
+              this.#updateQuality(i, 2)
+            } else {
+              this.#updateQuality(i, 1)
             }
           }
         }
       }
+
+      if (!this.#isAgedBrie(currentItem.name) && !this.#isBackstagePass(currentItem.name) && !this.#isSulfuras(currentItem.name) && currentItem.quality > 0) {
+        this.#updateQuality(i, -1)
+      }
+
       if (!this.#isSulfuras(currentItem.name)) {
         this.#updateSellIn(i, -1)
       }
+
       if (currentItem.sellIn < 0) {
         if (this.#isBackstagePass(currentItem.name)) {
           this.#updateQuality(i, -currentItem.quality)
