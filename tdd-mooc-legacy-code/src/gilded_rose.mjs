2379505f7453879fb736d721caeb12ea40cdcc52
pablo-maff <pablo.maff.076@gmail.com>
2024-05-15 22:05:20 +0200

    extracted updateQuality from updateItems

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -11,26 +11,30 @@ export class Shop {
     this.items = items;
   }
 
+  #updateQuality(i, value) {
+    this.items[i].quality = this.items[i].quality + value;
+  }
+
   updateItems() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.#updateQuality(i, -1)
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.#updateQuality(i, 1)
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.#updateQuality(i, 1)
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.#updateQuality(i, 1)
               }
             }
           }
@@ -44,15 +48,15 @@ export class Shop {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.#updateQuality(i, -1)
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.#updateQuality(i, -this.items[i].quality)
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.#updateQuality(i, this.items[i].quality)
           }
         }
       }
